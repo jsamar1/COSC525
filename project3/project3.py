08e134f3c514fb3b1ac1d788195795a5edaf0541
rtavasso1 <83459472+rtavasso1@users.noreply.github.com>
2022-04-01 20:25:46 -0400

    Update project3.py

--- project3/project3.py
+++ project3/project3.py
@@ -9,9 +9,10 @@ import os
 import PIL
 #from PIL import Image
 import tensorflow as tf
-import pathlib
+from tensorflow.keras.callbacks import TensorBoard
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
 
@@ -30,7 +31,7 @@ def load_image(filename, data_dir):
     return images
 
 
-def FullyConnected(task, lr):
+def FullyConnected(task, lr,num_epochs=10):
   if task=='race':
     n=7
     loss = 'categorical_crossentropy'
@@ -64,9 +65,12 @@ def FullyConnected(task, lr):
   Model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr),
                 loss=loss,
                 metrics=['accuracy'])
-  return Model, dataset_train, dataset_test
+  
+  history = Model.fit(dataset_train,validation_data=dataset_test, epochs=num_epochs)
+  
+  return history
 
-def Convolutional(task, lr):
+def Convolutional(task, lr, num_epochs=10):
   if task=='race':
     n=7
     loss = 'categorical_crossentropy'
@@ -101,9 +105,12 @@ def Convolutional(task, lr):
   convModel.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr),
                 loss=loss,
                 metrics=['accuracy'])
-  return convModel, dataset_train, dataset_test
+  
+  history = convModel.fit(dataset_train,validation_data=dataset_test, epochs=num_epochs)
+  
+  return history
 
-def Convolutional2(task, lr):
+def Convolutional2(task, lr, num_epochs=10):
   if task=='race':
     n=7
     loss = 'categorical_crossentropy'
@@ -143,10 +150,12 @@ def Convolutional2(task, lr):
                 loss=loss,
                 metrics=['accuracy'])
   
-  return conv2Model, dataset_train, dataset_test
+  history = conv2Model.fit(dataset_train,validation_data=dataset_test, epochs=num_epochs)
+  
+  return history
 
 def getLabels():
-    num = 1000
+    num = 2000
     train_labels = pd.read_csv('fairface_label_train.csv')
     file_train, age_train, gender_train, race_train = train_labels.iloc[:num,0].tolist(), train_labels.iloc[:num,1].to_numpy().reshape(-1,1), train_labels.iloc[:num,2].to_numpy(), train_labels.iloc[:num,3].to_numpy().reshape(-1,1)
     test_labels= pd.read_csv('fairface_label_val.csv')
@@ -157,29 +166,90 @@ def getLabels():
     race_train, race_test = OneHotEncoder(sparse=False).fit_transform(race_train), OneHotEncoder(sparse=False).fit_transform(race_test)
     return file_train, file_test, age_train, age_test, gender_train, gender_test, race_train, race_test
 
-
-
-file_train, file_test, age_train, age_test, gender_train, gender_test, race_train, race_test = getLabels()
+def task(num):
+    if num==1:
+        print(f'Training on Race:\n')
+        history_race = FullyConnected('race', 0.001, num_epochs=20)
+        print(f'\nTraining on Age:\n')
+        history_age = FullyConnected('age', 0.001, num_epochs=20)
+        print(f'\nTraining on Gender:\n')
+        history_gender = FullyConnected('gender', 0.001, num_epochs=20)
+    elif num==2:
+        print(f'Training on Race:\n')
+        history_race = Convolutional('race', 0.001, num_epochs=20)
+        print(f'\nTraining on Age:\n')
+        history_age = Convolutional('age', 0.001, num_epochs=20)
+        print(f'\nTraining on Gender:\n')
+        history_gender = Convolutional('gender', 0.001, num_epochs=20)
+    elif num==3:
+        print(f'Training on Race:\n')
+        history_race = Convolutional2('race', 0.001, num_epochs=40)
+        print(f'\nTraining on Age:\n')
+        history_age = Convolutional2('age', 0.001, num_epochs=40)
+        print(f'\nTraining on Gender:\n')
+        history_gender = Convolutional2('gender', 0.001, num_epochs=40)
+    #elif num==4:
+    
+    return history_race, history_age, history_gender
+        
+file_train, file_test, age_train, age_test, gender_train, gender_test, race_train, race_test = getLabels() # separates processed labels from csv
 data_dir_train = r"C:\Unreal Engine\Projects\COSC525\FromHub\project3\train_imgs\\"
 data_dir_test = r"C:\Unreal Engine\Projects\COSC525\FromHub\project3\val_imgs\\"
 
-images_train = load_image(file_train, data_dir_train)
+images_train = load_image(file_train, data_dir_train) # get image tensors corresponding to labels
 images_test = load_image(file_test, data_dir_test)
 
-
-#model, dataset_train, dataset_test = FullyConnected('race', 0.001)
-#model, dataset_train, dataset_test = Convolutional('race', 0.001)
-model, dataset_train, dataset_test = Convolutional2('gender', 0.1)
-
-
-history = model.fit(dataset_train,validation_data=dataset_test, epochs=10)
+# FC_race, FC_age, FC_gender = task(1)
+# Conv_race, Conv_age, Conv_gender = task(2)
+Conv2_race, Conv2_age, Conv2_gender = task(3)
   
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.show()
+def graph(history_race,history_age,history_gender, model):
+    plt.plot(history_race.history['accuracy'])
+    plt.plot(history_race.history['val_accuracy'])
+    plt.title('Race (' + model + ')')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+    plt.plot(history_race.history['loss'])
+    plt.plot(history_race.history['val_loss'])
+    plt.title('Race (' + model + ')')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(['train', 'val'], loc='upper right')
+    plt.show()
+    plt.plot(history_age.history['accuracy'])
+    plt.plot(history_age.history['val_accuracy'])
+    plt.title('Age (' + model + ')')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+    plt.plot(history_age.history['loss'])
+    plt.plot(history_age.history['val_loss'])
+    plt.title('Age (' + model + ')')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(['train', 'val'], loc='upper right')
+    plt.show()
+    plt.plot(history_gender.history['accuracy'])
+    plt.plot(history_gender.history['val_accuracy'])
+    plt.title('Gender (' + model + ')')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+    plt.plot(history_gender.history['loss'])
+    plt.plot(history_gender.history['val_loss'])
+    plt.title('Gender (' + model + ')')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(['train', 'val'], loc='upper right')
+    plt.show()
   
-  
-  
+# graph(FC_race,FC_age,FC_gender,'FC')
+# graph(Conv_race,Conv_age,Conv_gender,'Conv')
+graph(Conv2_race,Conv2_age,Conv2_gender,'Conv2')
   
   
   
