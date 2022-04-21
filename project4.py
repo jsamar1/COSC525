4d4614584b10f006cfcea48ad79e366c8b2b4933
rtavasso1 <83459472+rtavasso1@users.noreply.github.com>
2022-04-21 16:52:39 -0400

    Update project4.py

--- project4.py
+++ project4.py
@@ -59,19 +59,18 @@ def trainingData(window_size, stride, file_name='beatles.txt'):
         print('save')
         return x_enc,y_enc,enc
 
-def train(train_set,test_set,model,numEpochs,lr,temp=1):
+def train(x,y,model,numEpochs,lr,temp=1):
     model = model(temp)
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=lr), loss='categorical_crossentropy', metrics=['accuracy'])
     print(x.shape,y.shape)
-    history = model.fit(train_set,validation_data=test_set,epochs=numEpochs,verbose=1,callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss',patience=5)])
+    history = model.fit(x,y,validation_split=0.2,epochs=numEpochs,verbose=1,callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss',patience=5)])
     return history, model
 
 def predict(given,model,numOfChars,temp=1): # currently passing only the last generated sequence
     # model = model(temp)
     x = given
     for _ in range(numOfChars):
-
-        out = model(x[-1].reshape(2,5,NUMCHARS) , training=False).numpy()[-1].reshape(1,5,NUMCHARS) 
+        out = model(x, training=False).numpy()[-1].reshape(1,-1,NUMCHARS)  # new
         out = out.T==np.max(out.T,axis=0)
         out = out.T.astype('int')
         # if _ == 0:
@@ -104,8 +103,8 @@ def LSTM(temp):
     LSTM = keras.Sequential()
     LSTM.add(layers.LSTM(units=100, activation='tanh', return_sequences=True))
     LSTM.add(layers.Dropout(0.2))
-    LSTM.add(layers.LSTM(units=100, activation='tanh', return_sequences=True))
-    LSTM.add(layers.Dropout(0.2))
+    # LSTM.add(layers.LSTM(units=100, activation='tanh', return_sequences=True))
+    # LSTM.add(layers.Dropout(0.2))
 
     # LSTM.add(layers.LSTM(units=100, activation='tanh', return_sequences=True))
     # LSTM.add(layers.Dropout(0.2))
@@ -116,19 +115,15 @@ def LSTM(temp):
     return LSTM
 
 # def trainingData(window_size, stride, file_name='beatles.txt'):
-x,y,enc = trainingData(5,3)
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=42)
-train_set = tf.data.Dataset.from_tensor_slices((x,y))
-train_set = train_set.shuffle(buffer_size=1000).batch(64)
-test_set = tf.data.Dataset.from_tensor_slices((x_test,y_test))
-test_set = test_set.shuffle(buffer_size=1000).batch(64)
+x,y,enc = trainingData(10,5)
+
 # a,b,encoder = trainingData(10, 2)
 
 #hist = train(x,y,simpleRNN,numEpochs=5,lr=0.001)
-hist, lModel = train(train_set,test_set,LSTM,numEpochs=5,lr=0.003)
+hist, lModel = train(x,y,LSTM,numEpochs=7,lr=0.003)
 
 
-pred = predict(x[0].reshape(1,5,NUMCHARS),lModel,10,1)
+pred = predict(x[0].reshape(1,-1,NUMCHARS),lModel,10,1)
 print(pred)
 
 # hist, sModel = train(train_set,simpleRNN,numEpochs=5,lr=0.003)
